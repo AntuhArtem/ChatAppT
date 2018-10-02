669ab41c6a1b5ffcee093b018f8a746ca320f64c
Artem Antuh <artyomantuh423@mail.ru>
2018-10-02 22:17:49 +0300

    1 commit

---
+++ chatApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  chatApp
+//
+//  Created by Artem Antuh on 10/2/18.
+//  Copyright © 2018 Artem Antuh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+    }
+
+    @objc func handleLogout() {
+        let loginController = LoginController()
+        present(loginController, animated: true, completion: nil)
+    }
+}
+
